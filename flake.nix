b7e1354a29402c1fc2ba5bb53422a803c3c73974
Manuel Berkemeier <manuelbb@mail.uni-paderborn.com>
2022-06-24 16:09:37 +0200

    attempt i+1

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
     templates = {
       python-poetry2nix = {
         description = "A Python project built with poetry2nix";
-        path = ./python_poetry2nix2nix;
+        path = ./python_poetry2nix;
       };
     };
 
